2d4cdb521faf3ee3cc8bbdc0c5f98e5e13f16731
beingharisali <theharis99@gmail.com>
2026-07-02 16:39:15 +0500

    added create book controller

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 const getBooks = async (req, res) => {
   try {
     const result = await pool.query(`
-        SELECT books.id, books.title, books.ISBN, books.publish_year, authors.name AS author_name, publishers.name AS publisher_name FROM books
+        SELECT books.id, books.title, books.ISBN, books.publish_year, authors.id AS author_id, authors.name AS author_name, publishers.id AS publisher_id, publishers.name AS publisher_name FROM books
         JOIN authors ON books.author_id = authors.id JOIN publishers ON books.publisher_id = publishers.id
       `);
     res.status(StatusCodes.OK).json({
@@ -20,11 +20,25 @@ const getBooks = async (req, res) => {
 };
 const createBook = async (req, res) => {
   try {
-    res.status(201).json({
+    const { title, isbn, publish_year, author_id, publisher_id } = req.body;
+    const result = await pool.query(
+      `
+        INSERT INTO books (title, isbn, publish_year, author_id, publisher_id) 
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *
+      `,
+      [title, isbn, publish_year, author_id, publisher_id],
+    );
+
+    res.status(StatusCodes.CREATED).json({
       success: true,
+      book: result.rows[0],
     });
   } catch (error) {
-    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      msg: error.message,
+    });
   }
 };
 const updateBook = async (req, res) => {
